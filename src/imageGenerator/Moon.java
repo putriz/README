84c4ae28b62eceba45fddc87e88d0c17752dbff6
Zefanya Putri <zefanyanadia.putri@yahoo.com>
2016-12-08 14:34:49 -0500

    Modify Moon class

--- src/imageGenerator/Moon.java
+++ src/imageGenerator/Moon.java
@@ -1,7 +1,12 @@
 package imageGenerator;
 
-/**
- * Created by Jordae4 on 11/26/2016.
- */
-public class Moon {
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+
+public class Moon extends Circle {
+
+    public Moon(PApplet pApplet, ArrayList<Float> coordinates) {
+        super(pApplet,coordinates);
+    }
 }
